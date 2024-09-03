4e6592a98fc605784c5924f19a177e66a2b18c06
Monil Shah <monilshah1984@gmail.com>
2024-09-02 23:25:24 -0400

    Added Docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.9-slim
+
+COPY . /app
+
+WORKDIR /app
+
+RUN pip install --upgrade pip
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -124,5 +124,4 @@ def predict():
         return render_template("predict.html", title="Predict", form=form, output=str(e))
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    app.run(debug=True, host="0.0.0.0",port=5000)
